c8779482b9efa0026e96d4cdbe00108722637366
itscharlieb <charlielbloomfield@gmail.com>
2014-09-21 13:51:04 -0400

    Create GameBoard.java

---
+++ GameBoard.java
@@ -0,0 +1,28 @@
+public class GameBoard{
+
+	//There are a total of 9 squares on a tic tac toe board, making for a total of 9 possible moves
+	public static final int MAX_NUMBER_OF_MOVES = 9;
+
+	//Enum type fieldValue represents the three possible states a field can take
+	private Enum fieldValue {
+		Undeclared, Ex, Oh
+	}
+
+	//2D arraylist of fieldValues represents the game board
+	private fieldValue[][] board;
+
+	//constuctor creates the gameBoard
+	public GameBoard(){
+		board = fieldValue[3][3];
+		initializeBoard();
+	}
+
+	//initializes all board fields to Undeclared
+	private void initializeBoard(){
+		for(int i = 0; i<3; i++){
+			for(int j = 0; j<3; j++){
+				board[i][j] = Undeclared;
+			}
+		}
+	}
+}
